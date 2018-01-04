d2c57bfd38244b4d7e399032bce02da134eaa898
Adan Amarillas <aamarill.engr@gmail.com>
2018-01-03 21:38:14 -0800

    Create API for submitting events to a registered application

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
+  root to: 'welcome#index'
+
   devise_for :users
 
   resources :users do
     resources :registered_applications
   end
 
-
-  get 'welcome/index'
-  root to: 'welcome#index'
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 
 end
